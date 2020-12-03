15aade0464eb8729a6e7097b4f115209dec3f9e1
EnderFlop <enderflop@gmail.com>
2020-12-02 20:57:12 -0600

    day5 part2

--- 2015/day5.py
+++ 2015/day5.py
@@ -1,36 +1,25 @@
 with open("2015/day5.txt") as text_file:
   strings = text_file.read()
 
-def count_vowels(string):
-  vowels = 0
-  vowels += string.count("a")
-  vowels += string.count("e")
-  vowels += string.count("i")
-  vowels += string.count("o")
-  vowels += string.count("u")
-  return vowels
+def pair_check(string):
+  for index in range(len(string) - 1):
+    check_letters = string[index : index+2]
+    if check_letters in string.replace(check_letters, " ", 1):
+      print(f"{string} contains {check_letters} twice")
+      return True
 
-def double_letter(string):
-  alphabet_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-  for letter in alphabet_list:
-    if string.find(letter*2) != -1:
+def x_y_x(string):
+  for index in range(len(string) - 2):
+    if string[index] == string[index + 2]:
+      #print(f"Index {index} ({string[index]}) and index {index + 2} ({string[index + 2]}) are identical")
       return True
   return False
-
-def forbidden_strings(string):
-  forbidden_list = ["ab", "cd", "pq", "xy"]
-  for forbidden in forbidden_list:
-    if string.find(forbidden) != -1:
-      return False
-  return True
 
 strings = strings.split("\n")
 nice_strings = 0
 for string in strings:
-  if count_vowels(string) >= 3:
-    if double_letter(string):
-      if forbidden_strings(string):
-        print(f"{string} passes all tests")
-        nice_strings += 1
+  if pair_check(string):
+    if x_y_x(string):
+      #print(f"{string} passes all tests.")
+      nice_strings += 1
 print(nice_strings)
-
