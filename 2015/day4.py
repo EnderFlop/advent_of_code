63e64d17096d4ed02d4a11dd6d99f46f0926c126
EnderFlop <enderflop@gmail.com>
2020-12-02 19:47:14 -0600

    day4 part2

--- 2015/day4.py
+++ 2015/day4.py
@@ -1,11 +1,11 @@
 import hashlib
 
 
-for i in range(1000000):
+for i in range(1000000000000000):
   input = "yzbqklnj" + str(i)
   hash = hashlib.md5(input.encode())
   hex_code = hash.hexdigest()
-  if hex_code[0:5] == "00000":
+  if hex_code[0:6] == "000000":
     print(i)
     break
 
