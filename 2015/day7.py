22b3753fc9962567a7b00ed86b04ab043fd4d51b
EnderFlop <enderflop@gmail.com>
2020-12-03 17:18:06 -0600

    day7 part1
    https://github.com/ghaiklor/advent-of-code-2015/tree/master/day-07/part-2
    fuck this was hard

--- 2015/day7.py
+++ 2015/day7.py
@@ -1,48 +1,74 @@
+import re
+
 with open("2015/day7.txt") as text_file:
   instructions = text_file.read()
-
-wires_dict = {}
-
 instructions = instructions.split("\n")
-for instruction in instructions:
-  split_instruction = instruction.split()
 
-  target_wire = split_instruction[-1]
+def AND(a,b):
+  return a & b
 
-  if instruction == "0 -> c":
-    wires_dict["c"] = 0
-    continue
-  elif instruction == "1674 -> b":
-    wires_dict["b"] = 1674
-    continue
-  elif instruction == "lx -> a": 
-    if "lx" in wires_dict:
-      wires_dict["a"] = wires_dict["lx"]
-    continue
-  
-  elif split_instruction[0][0].isupper(): 
-    if split_instruction[1] in wires_dict:
-      wires_dict[target_wire] = ~ wires_dict[split_instruction[1]]
+def OR(a,b):
+  return a | b
 
-  elif split_instruction[0][0].islower() or split_instruction[0] == "1":
-    first_signal_wire = split_instruction[0]
-    command = split_instruction[1]
-    second_signal_wire = split_instruction[2]
-    if command == "AND":
-      if first_signal_wire in wires_dict and second_signal_wire in wires_dict:
-        wires_dict[target_wire] = first_signal_wire & second_signal_wire
-    elif command == "OR":
-      if first_signal_wire in wires_dict and second_signal_wire in wires_dict:
-        wires_dict[target_wire] = first_signal_wire | second_signal_wire
-    elif command == "RSHIFT":
-      if first_signal_wire in wires_dict:
-        wires_dict[target_wire] = first_signal_wire >> second_signal_wire #s_s_w in this case is a number to shift by
-    elif command == "LSHIFT":
-      if second_signal_wire in wires_dict:
-        print
-        wires_dict[target_wire] = first_signal_wire << second_signal_wire #s_s_w in this case is a number to shift by
+def NOT(a):
+  return ~a
 
-print(wires_dict)
+def LSHIFT(a,b):
+  return a << b
+
+def RSHIFT (a,b):
+  return a >> b
+
+def parse_instruction(instruction):
+  command = "".join(re.findall("[A-Z]", instruction))
+  args = re.findall(r"\w+", instruction.replace(command, ""))
+  destination = args.pop()
+  for index in range(len(args)):
+    try:
+      args[index] = int(args[index])
+    except ValueError:
+      continue
+  return command, args, destination
+
+wires = {}
+
+def calculateWire(wire_name):
+  wire = wires.get(wire_name, None)
+  print(wire_name, wire)
 
 
+  if isinstance(wire_name, int):
+    print(f"returning {wire_name}")
+    return wire_name
+  if isinstance(wire, int):
+    print(f"returning {wire}")
+    return wire
+  if wire is None:
+    print(f"returning none")
+    return None
+
+
+  if len(wire["command"]) > 1:
+    print(f"{wire['command']} in wire")
+    if wire["command"] == "OR":
+      wires[wire_name] = OR(calculateWire(wire["args"][0]), calculateWire(wire["args"][1]))
+    elif wire["command"] == "AND":
+      wires[wire_name] = AND(calculateWire(wire["args"][0]), calculateWire(wire["args"][1]))
+    elif wire["command"] == "NOT":
+      wires[wire_name] = NOT(calculateWire(wire["args"][0]))
+    elif wire["command"] == "LSHIFT":
+      wires[wire_name] = LSHIFT(calculateWire(wire["args"][0]), calculateWire(wire["args"][1]))
+    elif wire["command"] == "RSHIFT":
+      wires[wire_name] = RSHIFT(calculateWire(wire["args"][0]), calculateWire(wire["args"][1]))
+  else:
+    print(f"no command in wire")
+    wires[wire_name] = calculateWire(wire["args"][0])
+
+  return wires[wire_name]
     
+
+for instruction in instructions:
+  command, args, destination = parse_instruction(instruction)
+  wires[destination] = {"command": command, "args": args}
+
+print(calculateWire("a"))
