3b31cbae9c84459d62250196862439e1f6d804e7
EnderFlop <enderflop@gmail.com>
2020-12-05 23:52:38 -0600

    day6 part2

--- 2020/day6.py
+++ 2020/day6.py
@@ -1,4 +1,5 @@
 import re
+import string
 
 with open("2020/day6.txt") as text_file:
   text = text_file.read()
@@ -8,11 +9,17 @@ groups = text.split("\n\n")
 total_answers = 0
 for group in groups:
   person_list = group.split("\n")
-  answer_list = []
+  print(len(person_list))
+  answer_list = {}
   for person in person_list:
     for char in person:
       if char not in answer_list:
-        answer_list.append(char)
-  total_answers += len(answer_list)
+        answer_list[char] = 1
+      else:
+        answer_list[char] += 1
+  for value in answer_list.values():
+    
+    if value == len(person_list):
+      total_answers += 1
 
 print(total_answers)
