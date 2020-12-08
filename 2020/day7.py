03def43030336da83ef59fdd9b821632a81ec54f
EnderFlop <enderflop@gmail.com>
2020-12-08 00:04:06 -0600

    day7 part2 clearer

--- 2020/day7.py
+++ 2020/day7.py
@@ -19,7 +19,7 @@ for rule in text:
     container = container[2:]
     rules_dict.setdefault(bag, {})[container] = number
 
-def bag_search(string):
+def count_bags_inside(string):
   bags = rules_dict[string] #Dict of all bags in current bag
   count = 0 #init count
   if bags == {}: #If empty, return 0 because no bags are in it
@@ -28,13 +28,13 @@ def bag_search(string):
   print(f"bags = {bags}")
   for bag in bags: #For each bag
     print(f"adding {rules_dict[string][bag]}")
-    count += rules_dict[string][bag] #Add the initial amount of the bags
+    count += bags[bag] #Add amount of current bag
     print(f"going into {bag}")
-    count += rules_dict[string][bag] * bag_search(bag) #Add the amount of bags (the coefficient) * the amount of bags inside the bag to the count
+    count += bags[bag] * count_bags_inside(bag) #Add amount of current bag (the coefficient) * the amount of bags inside the bag to the count
   return count #return a count of all the bags inside the current bag
 
 print("\n\n\n\n\n")
-print(bag_search("shiny gold"))
+print(count_bags_inside("shiny gold"))
 
 #not 4444444444, too high
 #not 8140, too low
