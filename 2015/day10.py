d5855d7c09adc56578e650cd350a8dc494b95d5d
EnderFlop <enderflop@gmail.com>
2020-12-09 17:04:15 -0600

    day10 part2

--- 2015/day10.py
+++ 2015/day10.py
@@ -1,35 +1,17 @@
+from itertools import groupby
+
 input = "3113322113"
 #real input = "3113322113"
 
-def look_and_say(number, count):
-  #print(f"\nnumber = {number}, count = {count}")  
-  if count == 40:
-    return number
-  list = []
-  #Get lengths of numbers in a row
-  index = 0
-  row_length = 0
-  while number:
-    row_length += 1
-    #print(f"number = {number}. index = {index}. number[index] = {number[index]}")
-    try:
-      if number[index] == number[index + 1]:
-        index += 1
-      else:
-        append_string = str(row_length) + str(number[index])
-        list.append(append_string)
-        number = number[index+1:]
-        row_length = 0
-        index = 0
-    except IndexError:
-      list.append(str(row_length) + str(number[index]))
-      number = None
-      
-  s = [str(i) for i in list] 
-  res = "".join(s)
-  count += 1
-  return(look_and_say(res, count))
 
-number = look_and_say(input, 0)
-print(len(number))
+def look_and_say(number, loop_count):
+  for i in range(loop_count):
+    return_string = ""
+    print(f"loop #{i}")
+    for k, g in groupby(number):
+      return_string += str(len(list(g))) + str(k)
+    number = return_string
+  return number
+
+print(f"after 50 runs, input is {len(look_and_say(input, 50))} long")
   
