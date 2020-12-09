69302f03fd314dea3df4e1841ef3d5cb5c543a0a
EnderFlop <enderflop@gmail.com>
2020-12-08 23:12:40 -0600

    day9 part1

--- 2020/day9.py
+++ 2020/day9.py
@@ -1,8 +1,26 @@
 import re
 
 with open("2020/day9.txt") as text_file:
-  text = text_file
+  text = text_file.read()
 
 input = text.split("\n")
-for i in input:
-  print(i)
+for i in range(len(input)):
+  input[i] = int(input[i])
+preamble = input[:25]
+input = input[25:]
+
+def test(number):
+  for int_1 in preamble:
+    for int_2 in preamble:
+      #print(f"testing for {number}, {int_1} + {int_2} ({int_1 + int_2})")
+      if int_1 + int_2 == number and int_1 != int_2:
+        preamble.pop(0)
+        preamble.append(number)
+        return True
+  return False
+
+for number in input:
+  if not test(number):
+    print(number)
+    break
+  
