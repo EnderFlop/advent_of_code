0963214b1216c0d2afb49a3a765f1eceb16f6409
EnderFlop <enderflop@gmail.com>
2020-12-12 14:37:38 -0600

    day11 part2

--- 2020/day11.py
+++ 2020/day11.py
@@ -7,9 +7,6 @@ def step(text, seats): #Begin with a string of seats and floor
   return_text = ""
   lines = text.splitlines() #Get a list of the lines to iterate through
   text = text.replace("\n", "") #Remove the newlines for iteration purposes
-  board = "" #Long string that is used for index lookup of characters without needing to worry about newlines
-  for i in lines:
-    board += i
 
   seats_occupied = seats #Start with current seats occupied
   for row_index in range(len(lines)): #Row index = current row
@@ -19,32 +16,20 @@ def step(text, seats): #Begin with a string of seats and floor
       seat_number = seat_index + len(row) * row_index #Get the seat index in all of the text
       seat = row[seat_index] #Find the actual value of that seat
 
-      higher_row = [seat_number - len(row) - 1, seat_number - len(row), seat_number - len(row) + 1]
-      middle_row = [seat_number - 1, seat_number + 1] 
-      lower_row = [seat_number + len(row) - 1, seat_number + len(row), seat_number + len(row) + 1]
-
+      slopes = [(-1, -1), (0,-1), (1,-1), (-1,0), (1,0), (-1,1), (0,1), (1,1)] #The slopes to check
       actual_seats = []
-      for i in higher_row:
-        #Current problem. if your lower row is indexes 2,3,4 and the lines look like
-        # 0,1,2
-        # 3,4,5
-        # It will steal the value from the previous line instead of adding a period.
-        # SOLUTION: Impliment different checks for each row, using their row as a range check
-        # Add a check that i is in the current row by checking if i is in a range, starting with the current row's first index and ending with it's last
-        if i >= 0 and i < len(text) and i in range(len(row) * (row_index - 1), len(row) * row_index):
-          actual_seats.append(text[i])
-        else:
-          actual_seats.append(".")
-      for i in middle_row:
-        if i >= 0 and i < len(text) and i in range(len(row) * row_index, len(row) * (row_index + 1)):
-          actual_seats.append(text[i])
-        else:
-          actual_seats.append(".")
-      for i in lower_row:
-        if i >= 0 and i < len(text) and i in range(len(row) * (row_index + 1), len(row) * (row_index + 2)):
-          actual_seats.append(text[i])
-        else:
-          actual_seats.append(".")
+      for slope in slopes:
+        current_pos = [seat_index, row_index] #x, y of seat
+        while True:
+          current_pos[0] += slope[0] #Move by x of slope
+          current_pos[1] += slope[1] #Move by y of slope
+          if current_pos[0] < 0 or current_pos[0] >= len(row) or current_pos[1] < 0 or current_pos[1] >= len(lines): #if it is out of bounds, append an x
+            actual_seats.append("x")
+            break
+          current_seat = current_pos[0] + current_pos[1] * len(row) #If in bounds, find the position
+          if text[current_seat] == "#" or text[current_seat] == "L": #If it is either an open seat or a closed seat, append it and break
+            actual_seats.append(text[current_seat])
+            break
 
       number_of_occupied_adjacent_seats = actual_seats.count("#")
       #print(seat_number, actual_seats, number_of_occupied_adjacent_seats)
@@ -53,12 +38,12 @@ def step(text, seats): #Begin with a string of seats and floor
       elif seat == "L" and number_of_occupied_adjacent_seats == 0: #If the seat is open and there are no occupied seats nearby, occupy it.
         return_text += "#"
         seats_occupied += 1
-      elif seat == "#" and number_of_occupied_adjacent_seats >= 4: #If the seat is occupied and there are 4 or more adjacent seats, leave
+      elif seat == "#" and number_of_occupied_adjacent_seats >= 5: #If the seat is occupied and there are 4 or more adjacent seats, leave
         return_text += "L"
         seats_occupied -= 1
       elif seat == "L" and number_of_occupied_adjacent_seats >= 1: #If there is even 1 occupied seat near an open seat, keep it open
         return_text += "L"
-      elif seat == "#" and number_of_occupied_adjacent_seats < 4: #If the occupied seats around an occupied seat are less than 4, keep sitting
+      elif seat == "#" and number_of_occupied_adjacent_seats < 5: #If the occupied seats around an occupied seat are less than 4, keep sitting
         return_text += "#"
     return_text += "\n" #Add a newline after each line
   return return_text, seats_occupied
