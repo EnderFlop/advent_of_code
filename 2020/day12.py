6ab30dd86be0f8bba8857ee11ec39b0d135ba418
EnderFlop <enderflop@gmail.com>
2020-12-14 22:40:25 -0600

    day12 part2

--- 2020/day12.py
+++ 2020/day12.py
@@ -7,64 +7,70 @@ console = Console()
 
 directions = open("2020/day12.txt").read().splitlines()
 
-class Ship():
+class Waypoint_And_Ship():
   def __init__(self):
-    self.degrees_direction = 90 #In degrees. 0 = North, 90 = East, 180 = South, 270 = West
-    self.cardinal_direction = "E"
-    self.direction_dict = {
-      0: "N",
-      90: "E",
-      180: "S",
-      270: "W"
-    }
-    self.vertical_position = 0
-    self.horizontal_position = 0
+    self.waypoint_vert = 1
+    self.waypoint_hori = 10
+    self.ship_vert = 0
+    self.ship_hori = 0
 
   def run_command(self, command, units):
     console.log(f"RUNNING COMMAND {command} WITH UNITS {units}")
-    if command in ["F", "N", "E", "S", "W"]:
-      self.move(command, units)
+    if command in ["N", "E", "S", "W"]:
+      self.move_waypoint(command, units)
     elif command in ["R", "L"]:
-      self.rotate(command, units)
+      self.rotate_waypoint(command, units)
+    elif command in ["F"]:
+      self.move_ship(units)
     else:
       raise NameError(f"Unknown command issued to ship! COMMAND:{command} UNITS:{units}")
 
-  def change_cardinal(self):
-    self.cardinal_direction = self.direction_dict[self.degrees_direction % 360] #Mod 360 to keep it in a circle scale
+  def move_ship(self, moves):
+    #MOVE SHIP TOWARDS WAYPOINT {moves} AMOUNT OF TIMES
+    #ship coords += waypoint coords x times?
+    self.ship_hori += self.waypoint_hori * moves
+    self.ship_vert += self.waypoint_vert * moves
 
-  def move(self, direction, units): 
-    #If F, move in self.direction x units
-    #If anything else, move in that direction by that many units
-    if direction == "F": #Changes direction to the current direction the ship is facing
-      direction = self.cardinal_direction
-    
+  def move_waypoint(self, direction, units): 
     #VERTICAL ADJUSTERS
     if direction == "N":
-      self.vertical_position += units
+      self.waypoint_vert += units
     elif direction == "S":
-      self.vertical_position -= units
+      self.waypoint_vert -= units
     #HORIZONTAL ADJUSTERS
     elif direction == "E":
-      self.horizontal_position += units
+      self.waypoint_hori += units
     elif direction == "W":
-      self.horizontal_position -= units
+      self.waypoint_hori -= units
 
-  def rotate(self, direction, degrees):
-    #If right, positive. Left, negative.
-    if direction == "R":
-      self.degrees_direction += degrees
-      self.change_cardinal()
-    elif direction == "L":
-      self.degrees_direction -= degrees
-      self.change_cardinal()
+  def rotate_waypoint(self, direction, degrees):
+    #https://calcworkshop.com/wp-content/uploads/common-rotations-origin.png
+    if direction == "R": #CLOCKWISE
+      if degrees == 90:
+        self.waypoint_hori, self.waypoint_vert = self.waypoint_vert, (-1*self.waypoint_hori) #(x,y) -> (y,-x)
+      elif degrees == 180:
+        self.waypoint_hori, self.waypoint_vert = (-1*self.waypoint_hori), (-1*self.waypoint_vert) #(x,y) -> (-x,-y)
+      elif degrees == 270:
+        self.waypoint_hori, self.waypoint_vert = (-1*self.waypoint_vert), self.waypoint_hori #(x,y) -> (-y, x)
 
-ship = Ship()
+    elif direction == "L": #COUNTERCLOCKWISE
+      if degrees == 90:
+        self.waypoint_hori, self.waypoint_vert = (-1*self.waypoint_vert), self.waypoint_hori #(x,y) -> (-y, x)
+      elif degrees == 180:
+        self.waypoint_hori, self.waypoint_vert = (-1*self.waypoint_hori), (-1*self.waypoint_vert) #(x,y) -> (-x,-y)
+      elif degrees == 270:
+        self.waypoint_hori, self.waypoint_vert = self.waypoint_vert, (-1*self.waypoint_hori) #(x,y) -> (y,-x)
+
+
+ship = Waypoint_And_Ship()
 
 for i in directions:
   command = i[0]
   units = int(i[1:])
   ship.run_command(command, units)
 
-print(abs(ship.vertical_position) + abs(ship.horizontal_position))
+print(abs(ship.ship_hori) + abs(ship.ship_vert))
 #NOT 4789, too high
 #415! The degree direction was going way past 360 and it was tripping up the change_cardinal method
+
+#PART 2 29401 FIRST TRY!!!!!
