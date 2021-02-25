21d0210a318d42eae0044f0b418d19b1498026d2
EnderFlop <enderflop@gmail.com>
2021-02-24 22:36:32 -0600

    day12 part2

--- 2015/day12.py
+++ 2015/day12.py
@@ -5,6 +5,7 @@ import json
 with open("2015/day12.txt") as text_file:
   text = json.load(text_file)
 
+
 class Sum():
   def __init__(self):
     self.total = 0
@@ -16,16 +17,17 @@ class Sum():
       for i in data:
         self.add_inside(i)
     elif isinstance(data, dict):
-      for k,v in data.items():
-        if isinstance(k, int):
-          self.total += k
-        if isinstance(v, int):
-          self.total += v
-        if isinstance(v, list):
-          for item in v:
-            self.add_inside(item)
-        if isinstance(v, dict):
-          self.add_inside(v)
+      if "red" not in data.values():
+        for k,v in data.items():
+          if isinstance(k, int):
+            self.total += k
+          if isinstance(v, int):
+            self.total += v
+          if isinstance(v, list):
+            for item in v:
+              self.add_inside(item)
+          if isinstance(v, dict):
+            self.add_inside(v)
 
 part1 = Sum()
 part1.add_inside(text)
@@ -34,3 +36,4 @@ print(part1.total)
 #NOT 19835 FOR PART 1, ANSWER TOO LOW
 #NOT 2200320 EITHER, TOO HIGH
 #119433 FOR PART 1! 3RD TRY
+#68466 PART 2 FIRST TRY
