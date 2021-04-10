107327e41b02ccf215687fdf8e962f688b6b72aa
EnderFlop <enderflop@gmail.com>
2021-04-10 16:33:27 -0500

    day18 part2

--- 2020/day18.py
+++ 2020/day18.py
@@ -4,7 +4,7 @@ import numpy as np
 import time
 
 instructions = open("2020/day18.txt").read().splitlines()
-#instructions = ["2 * 3 + (4 * 5)"]
+#instructions = ["((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2"]
 
 def preform_operation(x, op, y):
   if op == "+":
@@ -14,7 +14,11 @@ def preform_operation(x, op, y):
 
 def evalutate_line(line):
   split_exp = line.split(" ")
-  x, op, y = split_exp[0], split_exp[1], split_exp[2]
+  try:
+    plus_location = split_exp.index("+")
+    x, op, y = split_exp[plus_location - 1], split_exp[plus_location], split_exp[plus_location + 1]
+  except ValueError:
+    x, op, y = split_exp[0], split_exp[1], split_exp[2]
   value = preform_operation(x, op, y)
   expression = line.replace(f"{x} {op} {y}", str(value), 1)
   return expression
@@ -42,3 +46,4 @@ for line in instructions:
 #29839238942436, too high.
 #29839238838303, second try. 
 # Was replacing every instance of the expression with the result, so 2*6 + 2 * 62 became (12) + (12)2
+#201376568795521 PART2 FIRST TRY
