66380675130bd85cd852debf312634c7aa23d12d
EnderFlop <enderflop@gmail.com>
2021-04-11 22:30:24 -0500

    day19 works on first example

--- 2020/day19.py
+++ 2020/day19.py
@@ -19,22 +19,42 @@ for rule in rules:
 #If the rule is only subrules, then the rule = the result of those sub rules as a list
 #If the rule is two possible sub-rules matches, then the result is a list of the results of those two sub-rules
 
+def distribute_list(list1, list2):
+  combined_list = []
+  for string1 in list1:
+    for string2 in list2:
+      combined_list.append(string1 + string2)
+  return combined_list
+
 def construct_rule(rule_number):
   related_rules = rules_dict[rule_number]
-  matches = []
   print(rule_number, related_rules)
   if related_rules[0] in ["\"b\"", "\"a\""]: #If the rule is just a character
-    return related_rules[0][1:-1] #Return that character
+    return [related_rules[0][1:-1]] #Return that character
   elif "|" in related_rules:
-    possibility_one = related_rules[0:2]
-    possibility_two = related_rules[3:]
+    possibility_one = related_rules[0:2] #First set of subrules
+    possibility_two = related_rules[3:] #Second set of subrules
+    first_set = []
+    second_set = []
+    for rule in possibility_one:
+      first_set += (construct_rule(rule))
+    first_set = "".join(first_set)
+    for rule in possibility_two:
+      second_set += (construct_rule(rule))
+    second_set = "".join(second_set)
+    print(first_set, second_set)
+    return [first_set, second_set]
   else:
-    end_string = ""
-    for related in related_rules:
-      end_string += construct_rule(related)
-    return end_string
-  return matches
+    returned_strings = []
+    for related in related_rules: #Construct the rules
+      returned_strings.append(construct_rule(related))
+    print(returned_strings)
+    if len(returned_strings) == 2:
+      return distribute_list(returned_strings[0], returned_strings[1])
+    if len(returned_strings) == 1:
+      return returned_strings
 
 
 possible_matches = construct_rule("0")
 print(possible_matches)
+
