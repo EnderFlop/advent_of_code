2a83a406613390b39689785def1cb49d0a2d37d2
EnderFlop <enderflop@gmail.com>
2021-11-24 00:01:47 -0600

    day2 part2

--- 2016/day2.py
+++ 2016/day2.py
@@ -2,22 +2,29 @@ import numpy as np
 
 input = open("2016/day2.txt").read().splitlines()
 
-keypad = np.array([[1,2,3],[4,5,6],[7,8,9]])
-keypad_x = 1
-keypad_y = 1
+keypad = np.array([["x", "x", 1, "x", "x"],["x", 2,3,4, "x"],[5,6,7,8,9],["x", "A","B","C", "x"],["x", "x", "D", "x", "x"]])
+keypad_x = 0
+keypad_y = 2
 buttons = []
+print(keypad[keypad_y][keypad_x])
+
 for i in input:
   for letter in i:
     print(letter)
-    if letter == "L" and keypad_x != 0:
+    if letter == "L" and keypad_x != 0 and keypad[keypad_y][keypad_x - 1] != "x":
       keypad_x -= 1
-    elif letter == "R" and keypad_x != 2:
+    elif letter == "R" and keypad_x != 4 and keypad[keypad_y][keypad_x + 1] != "x":
       keypad_x += 1
-    elif letter == "U" and keypad_y != 0: 
+    elif letter == "U" and keypad_y != 0 and keypad[keypad_y - 1][keypad_x] != "x":
       keypad_y -= 1
-    elif letter == "D" and keypad_y != 2:
+    elif letter == "D" and keypad_y != 4 and keypad[keypad_y + 1][keypad_x] != "x":
       keypad_y += 1
+  print(keypad[keypad_y][keypad_x])
   buttons.append(keypad[keypad_y][keypad_x])
 
 print(buttons)
 #part one 98575 first try
+#part two CD8D4 first try
+#this is some shitty code, but I don't know enough about numpy to put an array at a specific point on top of another array without just using junk to place it there
+#I don't think there is any, that's why numpy can fill your array with periods or zeros.
+#There's still a better way to check, but this works for my use case. 
