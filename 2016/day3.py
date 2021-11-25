59d5df8d12d4c8cc0a40cacf5f7ed24f22a7263c
EnderFlop <enderflop@gmail.com>
2021-11-24 18:09:39 -0600

    day3 part2

--- 2016/day3.py
+++ 2016/day3.py
@@ -5,11 +5,24 @@ import time
 
 instructions = open("2016/day3.txt").read().splitlines()
 
-valid_triangles = 0
-for triangle in instructions:
-  side1, side2, side3 = triangle.split()
+fake_array = []
+for row in instructions:
+  side1, side2, side3 = row.split()
   side1, side2, side3 = int(side1), int(side2), int(side3)
-  if side1 + side2 > side3 and side1 + side3 > side2 and side2 + side3 > side1:
-    valid_triangles += 1
-  
-#983, second try (forgot to convert to int lol)
+  fake_array.append([side1, side2, side3])
+
+triangle_map = np.asarray(fake_array)
+valid_triangles = 0
+
+y_pointer = 0
+while y_pointer < len(triangle_map):
+  for x_pointer in range(3):
+    side1, side2, side3 = triangle_map[y_pointer][x_pointer], triangle_map[y_pointer + 1][x_pointer], triangle_map[y_pointer + 2][x_pointer]
+    if side1 + side2 > side3 and side1 + side3 > side2 and side2 + side3 > side1:
+      valid_triangles += 1
+  y_pointer += 3
+
+print(valid_triangles)
+
+#part1 983, second try (forgot to convert to int lol)
+#part2 1836, first try! Array manipulation is actually kinda fun lol. surprised I got the pointer math right the first try
