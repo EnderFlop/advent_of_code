2943718d89d602a9e5952ec4d6be8fd225cab300
EnderFlop <enderflop@gmail.com>
2021-12-03 13:17:56 -0600

    day3 part2

--- 2021/day3.py
+++ 2021/day3.py
@@ -13,26 +13,47 @@ def binaryToDecimal(n):
 
 grid = [[i for i in x] for x in instructions] #double list comprehension!
 grid = np.asarray(grid)
-gamma_bit_list = []
-epsilon_bit_list = []
-for column in range(len(grid[0])):
+
+#valid_list = all rows in grid
+#for every column index.
+  #for every row in the valid_list.
+    #Scan the column index bit of every row and see which one is larger.
+    #Add the row to a "ones_list" or a "zeroes_list"
+  #If there are more ones, or equal zeroes and ones
+    #continue onto the next column with valid_list = "ones_list"
+  #if there are more zeros
+    #continue onto the next column with valid_list = "zeroes_list"
+  
+
+valid_rows = [list(grid[i]) for i in range(len(grid))]
+for column_index in range(len(grid[0])):
   zero_count = 0
   one_count = 0
-  for unit in grid[:,column]:
-    if unit == "0":
+  zeroes_list = []
+  ones_list = []
+  if len(valid_rows) == 1:
+    break
+  for row in valid_rows:
+    current_bit = row[column_index]
+    if current_bit == "0":
       zero_count += 1
-    elif unit == "1":
+      zeroes_list.append(row)
+    elif current_bit == "1":
       one_count += 1
-  if zero_count > one_count:
-    gamma_bit_list.append(0)
-    epsilon_bit_list.append(1)
-  elif one_count > zero_count:
-    gamma_bit_list.append(1)
-    epsilon_bit_list.append(0)
+      ones_list.append(row)
+  if one_count < zero_count:
+    valid_rows = ones_list
+  elif zero_count <= one_count:
+    valid_rows = zeroes_list
 
-gamma_str = "".join([str(i) for i in gamma_bit_list])
-epsilon_str = "".join([str(i) for i in epsilon_bit_list])
-print(f"Gamma: {binaryToDecimal(gamma_str)}. Epsilon: {binaryToDecimal(epsilon_str)}")
-print(f"Multiplied: {binaryToDecimal(gamma_str) * binaryToDecimal(epsilon_str)}")
+co2_str = "".join(valid_rows[0])
+co2_rating = binaryToDecimal(co2_str)
+print(co2_rating)
+
+#OXYGEN_RATING = 3089
+#CO2_RATING = 257
+print(3089 * 257)
+
 #part1 3827 too low. Just realized I didnt even read the second half of the puzzle. Should be pretty easy from here.
 #part1 1025636. Just had to actually finish the problem.
+#part2 793873. first try! feels ugly that I had to change the program to get the two different values, but all I did was change the greater thans etc.
