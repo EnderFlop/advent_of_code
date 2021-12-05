492887cda65f7be9a61a69bdac9a4c11d4d92e92
EnderFlop <enderflop@gmail.com>
2021-12-05 14:38:38 -0600

    day5 part2

--- 2021/day5.py
+++ 2021/day5.py
@@ -30,12 +30,34 @@ for coords in coord_sets:
     difference = abs(y1 - y2)
     for i in range(0, difference + 1):
       grid[lower_y + i][x1] += 1
-  if y1 == y2:
+  elif y1 == y2:
     lower_x = min(x1, x2)
     higher_x = max(x1, x2)
     difference = abs(x1 - x2)
     for i in range(0, difference + 1):
       grid[y1][lower_x + i] += 1
+  #now for the work on diagonal lines
+  elif x1 != x2 and y1 != y2:
+    points = []
+    #the abs(x1 - x2) and abs(y1 - y2) are always the same, so it will always be a straight diagonal.
+    #take (8,0) to (0,8). walking from x=8 to x=0 means the range must be from 8 to zero
+    if x1 > x2 and y1 > y2:
+      #walk bottom right to top left
+      points = list(zip(range(y1, y2 - 1, -1), range(x1, x2 - 1, -1)))
+    elif x1 > x2 and y1 < y2:
+      #walk top right to bottom left
+      points = list(zip(range(y1, y2 + 1), range(x1, x2 -1, -1)))
+    elif x1 < x2 and y1 > y2:
+      #walk bottom left to top right
+      points = list(zip(range(y1, y2 - 1, -1), range(x1, x2 + 1)))
+    elif x1 < x2 and y1 < y2:
+      #walk top left to bottom right
+      points = list(zip(range(y1, y2 + 1), range(x1, x2 + 1)))
+    for p in points:
+      y, x = p
+      grid[y][x] += 1
+      
+
 
 collision_count = 0
 for row in grid:
@@ -45,3 +67,5 @@ for row in grid:
 
 print(collision_count)
 #4993 first try!
+#21101 first try! thank god, this would have kicked my ass to bug fix. 
+# i hate index math :) especially when I design it so the x and y are backwards :) and big y means down :))))
