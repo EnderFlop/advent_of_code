277f6c1c60ab23e325c1d16ca49ffcd489f38e4e
EnderFlop <enderflop@gmail.com>
2021-12-12 12:57:21 -0600

    day12 part1

--- 2021/day12.py
+++ 2021/day12.py
@@ -2,66 +2,85 @@ import re
 import itertools
 import numpy as np
 import time
+from collections import defaultdict
 
-instructions = open("2021/day12testinput.txt").read().splitlines()
-#instructions = open("2021/day12input.txt").read().splitlines()
+#instructions = open("2021/day12testinput.txt").read().splitlines()
+instructions = open("2021/day12input.txt").read().splitlines()
+
+
+# This class represents a directed graph
+# using adjacency list representation
 
 class Node():
-  def __init__(self, name, paths = []):
+  def __init__(self, name):
     self.name = name
-    self.paths = []
     self.big = self.name.isupper()
     self.times_visited = 0
-  
+
   def __repr__(self):
     return self.name
-  
-  def add_path(self, node):
-    self.paths.append(node)
-  
-  def get_paths(self):
-    return self.paths
 
+class Graph:
+  
+    def __init__(self, vertices):
+      # No. of vertices
+      self.V = vertices
+        
+      # default dictionary to store graph
+      self.graph = {}
+
+      self.path = []
+
+      self.path_count = 0
+  
+    # function to add an edge to graph
+    def addEdge(self, u, v):
+      if u.name not in self.graph.keys():
+        self.graph[u.name] = []
+      self.graph[u.name].append(v)
+  
+    '''A recursive function to print all paths from 'u' to 'd'.
+    visited[] keeps track of vertices in current path.
+    path[] stores actual vertices and path_index is current
+    index in path[]'''
+    def dfs(self, start, end):
+
+      current_node = nodes[start.name]
+
+      # Mark the current node as visited and store in path
+      current_node.times_visited += 1
+      self.path.append(current_node)
+
+      # If current vertex is same as destination, then print
+      # current path[]
+      if start.name == end.name:
+        self.path_count += 1
+        #print(self.path)
+      else:
+        # If current vertex is not destination
+        # Recur for all the vertices adjacent to this vertex
+        for i in self.graph[start.name]:
+          i = nodes[i.name]
+          # small nodes can only be visited once, big nodes can be visited however often
+          if (i.times_visited == 0 and i.big == False):
+            self.dfs(i, end)
+          elif (i.big == True):
+            self.dfs(i, end)
+      self.path.pop()
+      current_node.times_visited -= 1
+
+graph = Graph(len(instructions))
 nodes = {}
 for i in instructions:
   start, end = i.split("-")
-  if start not in nodes.keys():
-    nodes[start] = Node(start)
-  if end not in nodes.keys():
-    nodes[end] = Node(end)
-  nodes[start].add_path(nodes[end])
-  nodes[end].add_path(nodes[start])
+  start = Node(start)
+  if start.name not in nodes.keys():
+    nodes[start.name] = start
+  end = Node(end)
+  if end.name not in nodes.keys():
+    nodes[end.name] = end
+  graph.addEdge(start, end)
+  graph.addEdge(end, start)
 
-#run a depth first search on start, and get all possible paths.
-
-def printAllPathsUtil(self, u, d, visited, path):
- 
-  # Mark the current node as visited and store in path
-  visited[u]= True
-  path.append(u)
-
-  # If current vertex is same as destination, then print
-  # current path[]
-  if u == d:
-      print path
-  else:
-      # If current vertex is not destination
-      # Recur for all the vertices adjacent to this vertex
-      for i in self.graph[u]:
-          if visited[i]== False:
-              self.printAllPathsUtil(i, d, visited, path)
-                
-  # Remove current vertex from path[] and mark it as unvisited
-  path.pop()
-  visited[u]= False
-
-def printAllPaths(self, s, d):
- 
-  # Mark all the vertices as not visited
-  visited =[False]*(self.V)
-
-  # Create an array to store paths
-  path = []
-
-  # Call the recursive helper function to print all paths
-  self.printAllPathsUtil(s, d, visited, path)
+graph.dfs(nodes["start"], nodes["end"])
+print(graph.path_count)
