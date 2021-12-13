a2cd02d7baeceea3524649c71e6599f8f93ae6f1
EnderFlop <enderflop@gmail.com>
2021-12-13 11:54:34 -0600

    day13 part2 progress

--- 2021/day13.py
+++ 2021/day13.py
@@ -26,8 +26,6 @@ for i in instructions:
       max_y = y
     coords.append((x, y))
 
-print(coords)
-print(folds)
 
 #grid max_x long and max_y high.
 grid = [["." for x in range(max_x + 1)] for y in range(max_y + 1)]
@@ -36,32 +34,35 @@ grid = np.asarray(grid)
 for x, y in coords:
   grid[y][x] = "#"
 
-folds = [folds[0]]
-
 for fold in folds:
   a, index = fold.split("=")
   axis = a[-1]
   index = int(index)
+
   if axis == "y":
     grid_one, grid_two = np.split(grid, axis=0, indices_or_sections=[index])
     grid_two = np.flipud(grid_two)
     grid_two = grid_two[:-1]
+
   if axis == "x":
     grid_one, grid_two = np.split(grid, axis=1, indices_or_sections=[index])
     grid_two = np.fliplr(grid_two)
     grid_two = grid_two[:,[x for x in range(grid_two.shape[1] - 1)]]
+  
   for row_index, row in enumerate(grid_two):
-      for point_index, point in enumerate(row):
-        if point == "#":
-          grid_one[row_index][point_index] = "#"
+    for point_index, point in enumerate(row):
+      if point == "#":
+        grid_one[row_index][point_index] = "#"
   grid = grid_one
 
-dot_count = 0
 for row_index, row in enumerate(grid):
+  row_str = ""
   for point_index, point in enumerate(row):
+    if point == ".":
+      row_str += " "
     if point == "#":
-      dot_count += 1
-
-print(dot_count)
+      row_str += "#"
+  print(row_str)
   
 #759 part 1 first try!
+#HOGAZKPR not right, i just tried to guess the blurry letters. something is going wrong
