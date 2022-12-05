013da2dc83e579505df6e9f047f038d1962d23aa
enderflop <agbarloon@uiowa.edu>
2022-12-05 15:25:42 -0600

    2022 day3 part2

--- 2022/day3.py
+++ 2022/day3.py
@@ -8,12 +8,15 @@ import string
 instructions = open("2022/day3input.txt").read().splitlines()
 
 letter_lookup = string.ascii_letters
+i = 0
 priorities = 0
-for rucksack in instructions:
-  ruck_len = len(rucksack)
-  one, two = rucksack[:ruck_len//2], rucksack[ruck_len//2:]
-  shared_items = list(set([char for char in one if char in two]))
+while i < len(instructions):
+  one, two, three = instructions[i], instructions[i + 1], instructions[i + 2]
+  shared_items = list(set([char for char in one if char in two and char in three]))
+  print(shared_items)
   priorities += letter_lookup.index(shared_items[0]) + 1
+  i += 3
 
 print(priorities)
 #part1 7746 first try!
+#part2 2604 first try!
