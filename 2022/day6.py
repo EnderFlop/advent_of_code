4b389a2052c53aaae4ebf2348b36322fe7df2bbc
enderflop <agbarloon@uiowa.edu>
2022-12-06 12:09:01 -0600

    2022 day6 part2

--- 2022/day6.py
+++ 2022/day6.py
@@ -8,9 +8,10 @@ signal = open("2022/day6input.txt").read()
 
 def detect_start(signal):
   for i in range(len(signal)):
-    packet = signal[i:i+4]
-    if len(set(packet)) == 4:
-      return i + 4
+    packet = signal[i:i+14]
+    if len(set(packet)) == 14:
+      return i + 14
 
 print(detect_start(signal))
 #part1 1100 first try! can't believe wrapping a string in set() does exactly what i wanted it to, isolate the letters and remove duplicates!
+#part2 2421 first try! super fast and easy change. guess that means it's good code huh, very modular
