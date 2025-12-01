589eef536db0c75c682514e21e8e24c7ea3919f7
enderflop@gmail.com <agbarloon@uiowa.edu>
2025-12-01 12:51:30 -0600

    2025 day1 part2

--- 2025/day1.py
+++ 2025/day1.py
@@ -10,14 +10,37 @@ total = 0
 i = 50
 
 for instruction in instructions:
+  print(instruction)
+  print("B", i)
+
   direction = instruction[0]
   rotation = int(instruction[1:])
+
+  #R1000 points at 0 ten times.
+  if rotation // 100:
+    print("spin" * (rotation // 100))
+    total += rotation // 100 #add any full rotations
+
+  remaining_rotation = rotation % 100
+
   if direction == "R":
-    i += rotation
+    new_i = i + remaining_rotation
   elif direction == "L":
-    i -= rotation
-  i %= 100
-  if i == 0:
+    new_i = i - remaining_rotation
+
+  if i != 0 and (new_i < 0 or new_i > 100):
+    print("trigger")
     total += 1
 
+  i = new_i % 100
+  print("F", i)
+  if i == 0:
+    print("land")
+    total += 1 
+
+
 print(total)
+
+#2577 too low
+#6246 too high
+#6027 correct
