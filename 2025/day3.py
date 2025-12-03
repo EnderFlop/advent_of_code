7f6ab35a3bdb0467e053215d3f597f0d0eb42248
enderflop@gmail.com <agbarloon@uiowa.edu>
2025-12-03 10:42:14 -0600

    2025 day3 part2 sliding index solution

--- 2025/day3.py
+++ 2025/day3.py
@@ -7,18 +7,36 @@ import time
 instructions = open("2025/day3input.txt").read().splitlines()
 
 total = 0
+
 for i in instructions:
-  left = 0
-  best = 0
+  indexes = [j for j in range(len(i) - 12, len(i))]
+  tracker = 0
+  #loop
+  while tracker != 12:
 
-  while left != len(i) - 1:
-    l = i[left]
-    r = max(i[left + 1:])
-    res = int(str(l) + str(r))
-    best = max(res, best)
-    left += 1
+    current_val = int(i[indexes[tracker]])
+    left_offset = 1
+    while indexes[tracker] - left_offset >= 0: #while index in range
 
-  total += best
+      if tracker > 0 and (indexes[tracker] - left_offset) <= indexes[tracker - 1]: #if this index is already directly on top of the one below it, continue
+        break
+
+      new_val = int(i[indexes[tracker] - left_offset])
+
+      if new_val >= current_val:
+        current_val = new_val
+        indexes[tracker] -= left_offset
+        left_offset = 0
+
+      left_offset += 1
+    
+    tracker += 1
+
+  battery_total = int("".join(i[k] for k in indexes))
+  total += battery_total
 
 print(total)
+
 #17694 ans part 1
+
+#175659236361660 ans part 2
