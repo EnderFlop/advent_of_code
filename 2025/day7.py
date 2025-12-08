672f0bbc596b86c08003ae4d6ddd6dbef9c76ce2
agbarloon <agbarloon@uiowa.edu>
2025-12-08 15:21:42 +0100

    2025 day7 part2

--- 2025/day7.py
+++ 2025/day7.py
@@ -1,40 +1,34 @@
 import re
 import itertools
 import numpy as np
-import time
-
-#instructions = open("2025/day7testinput.txt").read().splitlines()
-instructions = open("2025/day7input.txt").read().splitlines()
+from collections import defaultdict
+instructions = open("2025/day7testinput.txt").read().splitlines()
+#instructions = open("2025/day7input.txt").read().splitlines()
 
 #get beam indexes (all | or S in row)
 #cast beam beneath (read row below)
 #   if index below is a ., turn to a |
 #   if index below is a ^, turn left and right into |
 
-def insert_char(string, insert, index):
-    return string[:index] + insert + string[index + 1:]
+beams = {instructions[0].find("S"): 1}
 
-total = 0
-
-for row_idx in range(len(instructions[:-1])):
+for row_idx in range(1, len(instructions)):
     row = instructions[row_idx]
-    indexes = [idx for idx, elem in enumerate(row) if elem == "|" or elem == "S"]
-    next_row = instructions[row_idx + 1]
-    for beam in indexes:
+    new_beams = defaultdict(int)
+    for beam, count in beams.items():
         #if empty, turn to beam
-        if next_row[beam] == ".":
-            next_row = insert_char(next_row, "|", beam)
+        if row[beam] == ".":
+            new_beams[beam] += count
         #if splitter, split
-        if next_row[beam] == "^":
-            total += 1
+        if row[beam] == "^":
             if beam - 1 >= 0:
-                next_row = insert_char(next_row, "|", beam - 1) 
-            if beam + 1 < len(next_row):
-                next_row = insert_char(next_row, "|", beam + 1)
-        instructions[row_idx + 1] = next_row
+                new_beams[beam - 1] += count
+            if beam + 1 < len(row):
+                new_beams[beam + 1] += count
+    beams = new_beams
 
-#print(instructions)
-#print(instructions[-1].count("|"))
-print(total)
-
+print(sum(beams.values()))
 #1546 part1 answer
+
+#13883459503480 part2 answer
+# had to pivot from recursive backtracking approach, even memoization couldn't save it
