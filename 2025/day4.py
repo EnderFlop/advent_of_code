b8f26146c6c2fea126da36aaba87750827eaa512
agbarloon <agbarloon@uiowa.edu>
2025-12-08 13:39:29 +0100

    2025 day4 part2

--- 2025/day4.py
+++ 2025/day4.py
@@ -6,6 +6,10 @@ import time
 #instructions = open("2025/day4testinput.txt").read().splitlines()
 instructions = open("2025/day4input.txt").read().splitlines()
 
+def modify_grid(grid, pairs):
+    for y, x in pairs:
+        grid[y] = grid[y][:x] + "." + grid[y][x + 1:]
+
 def check_neighbors(grid, r, c):
     directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
     count = 0
@@ -18,15 +22,27 @@ def check_neighbors(grid, r, c):
     return count
 
 reachable = 0
+new_reachable = 0
+while True:
 
-for r, row in enumerate(instructions):
-    for c, val in enumerate(row):
-        if instructions[r][c] == ".":
-            continue
+    valid_rolls = []
+    for r, row in enumerate(instructions):
+        for c, val in enumerate(row):
+            if instructions[r][c] == ".":
+                continue
 
-        if check_neighbors(instructions, r, c) < 4:
-            reachable += 1
+            if check_neighbors(instructions, r, c) < 4:
+                new_reachable += 1
+                valid_rolls.append((r, c))
+    
+    if new_reachable == reachable: #if total did not change (no new paper was removed)
+        break
+
+    else:
+        modify_grid(instructions, valid_rolls)
+        reachable = new_reachable
 
 print(reachable)
 
-#1393 day1 answer
+#1393 part1 answer
+#8643 part2 answer
